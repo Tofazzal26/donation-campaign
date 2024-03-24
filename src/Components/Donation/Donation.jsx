a641807831b97dba834da9c2e13db6cea8107fd1
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-24 23:30:02 +0600

    donation show all and show less validation finish

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import useLocalStorage from "../../Hooks/useLocalStorage";
 import DonationCard from "../DonationCard/DonationCard";
 
 const Donation = () => {
   const { localData } = useLocalStorage();
 
+  const [toggle, setToggle] = useState(false);
+
+  const handleSeeAll = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div className="container mx-auto">
       <div className="grid lg:grid-cols-2 gap-4 mt-12">
-        {localData.map((items) => (
+        {localData.slice(0, toggle ? localData.length : 4).map((items) => (
           <DonationCard key={items.id} items={items} />
         ))}
+      </div>
+      <div className="text-center mt-6">
+        <button
+          onClick={handleSeeAll}
+          className="bg-[#009444] px-6 py-3 font-semibold text-white rounded-md"
+        >
+          {!toggle ? "Show All" : "Show Less"}
+        </button>
       </div>
     </div>
   );
